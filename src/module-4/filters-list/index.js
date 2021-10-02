f71b8cad652bcdeb904692f8b4adfc7ff1d8975b
ultrayulenka <cherevata.jul@gmail.com>
2021-10-02 19:58:55 +0300

    Completed module-4 tasks

--- src/module-4/filters-list/index.js
+++ src/module-4/filters-list/index.js
@@ -1,11 +1,88 @@
 export default class FiltersList {
+  element;
+  listElement;
+  inputElements;
+  className = "filter-item";
+  listClassName = "options-list";
+
   constructor ({
      title = '',
-     list = []
+     list = [],
+     tag = ''
   } = {}) {
     this.title = title;
     this.list = list;
+    this.tag = tag;
 
-    // ... your logic
+    this.render();
+    this.addEvents();
+  }
+
+  render () {
+    this.element = document.createElement(this.tag? this.tag : "div");
+    this.element.className = this.className;
+    this.element.innerHTML = `<h4 class="filter-item__title">${this.title}</h4>`;
+    if(this.list.length) this.renderList();
+  }
+
+  renderList () {
+    this.listElement = document.createElement("ul");
+    this.listElement.className = this.listClassName;
+    this.list.forEach(this.renderListItem);
+    this.element.append(this.listElement);
+    this.inputElements = this.element.querySelectorAll("input");
+  }
+
+  renderListItem = (item) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `<li class="options-list__item">
+      <div>
+        <input class="options-list__input" type="checkbox" id="${item.value}" 
+        value="${item.value}" name="${item.title}" ${item.checked? "checked" : ""}>
+        <label class="options-list__label" for="${item.value}">${item.title}</label>
+      </div>
+    </li>`
+    this.listElement.append(wrapper.firstElementChild);
+  }
+
+  addEvents () {
+    this.element.addEventListener("change", this.onChange);
+  }
+
+  removeEvents () {
+    this.element.removeEventListener("change", this.onChange);
+  }
+
+  onChange = (event) => {
+    if(event.target.type !== "checkbox") return;
+    let newEvent;
+    if(event.target.checked) {
+      newEvent = new CustomEvent("add-filter", { bubbles: true, detail: event.target.value });
+    } else {
+      newEvent = new CustomEvent("remove-filter", { bubbles: true, detail: event.target.value });
+    }
+    event.target.dispatchEvent(newEvent);
+  }
+
+  reset = () => {
+    this.inputElements.forEach(input => {
+      if(input.checked) {
+        input.checked = false;
+      }
+    });
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+      this.removeEvents();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+    this.listElement = null;
+    this.inputElements = null;
   }
 }
--- src/module-4/side-bar/index.js
+++ src/module-4/side-bar/index.js
@@ -1,10 +1,112 @@
 import FiltersList from '../filters-list/index.js';
 
 export default class SideBar {
+  element;
+  listElement;
+  filters = [];
+  btnClear;
+  selectedFilters = [];
+
   constructor (categoriesFilter = [], brandFilter = []) {
     this.categoriesFilter = categoriesFilter;
     this.brandFilter = brandFilter;
 
-    // ... your logic
+    this.render();
+    this.addEvents();
+  }
+
+  get template() {
+    return `<div class="sidebar__header">
+        <h3 class="section-title">
+            Filters
+        </h3>
+        <button class="button button_small">
+            <div class="arrows arrows_left"></div>
+            <div class="arrows arrows_down"></div>
+        </button>
+    </div>
+    <div class="sidebar__container">
+        <ul class="filter-list"></ul>
+    </div>
+    <button class="button button_primary button_large" id="clear-filters">
+        CLEAR ALL FILTERS
+    </button>`
+  }
+
+  render () {
+    this.element = document.createElement("aside");
+    this.element.className = "sidebar";
+    this.element.innerHTML = this.template;
+    this.listElement = this.element.querySelector("ul");
+    this.btnClear = this.element.querySelector("#clear-filters");
+
+    if(this.categoriesFilter.length) this.renderFilter({title: "Category", list: this.categoriesFilter});
+    if(this.brandFilter.length) this.renderFilter({title: "Brand", list: this.brandFilter});
+  }
+
+  addEvents () {
+    this.btnClear.addEventListener("click", this.onClearFiltersClick);
+    this.element.addEventListener("add-filter", this.onAddFilter);
+    this.element.addEventListener("remove-filter", this.onRemoveFilter);
+  }
+
+  removeEvents () {
+    this.btnClear.removeEventListener("click", this.onClearFiltersClick);
+    this.element.removeEventListener("add-filter", this.onAddFilter);
+    this.element.removeEventListener("remove-filter", this.onRemoveFilter);
+  }
+
+  onClearFiltersClick = () => {
+    this.selectedFilters = [];
+    this.resetFilters();
+    const newEvent = new CustomEvent("clear-filters", { bubbles: true });
+    this.element.dispatchEvent(newEvent);
+  }
+
+  onAddFilter = (event) => {
+    this.selectedFilters.push(event.detail);
+    const newEvent = new CustomEvent("filter-selected", { bubbles: true, detail: this.selectedFilters });
+    this.element.dispatchEvent(newEvent);
+  }
+
+  onRemoveFilter = (event) => {
+    const index = this.selectedFilters.findIndex(item => item === event.detail);
+    if(index < 0) return;
+
+    this.selectedFilters.splice(index, 1);
+
+    let newEvent;
+    if(this.selectedFilters.length === 0) {
+      newEvent = new CustomEvent("clear-filters", { bubbles: true });
+    } else {
+      newEvent = new CustomEvent("filter-selected", { bubbles: true, detail: this.selectedFilters });
+    }
+    this.element.dispatchEvent(newEvent);
+  }
+
+  resetFilters () {
+    this.filters.forEach(filterItem => filterItem.reset());
+  }
+
+  renderFilter ({list, title = ""}) {
+    const filterItem = new FiltersList({ list, title, tag: "li" });
+    this.filters.push(filterItem);
+    this.listElement.append(filterItem.element);
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+      this.removeEvents();
+    }
+  }
+
+  destroy () {
+    this.remove();
+    this.element = null;
+    this.listElement = null;
+    this.filters = [];
+    this.btnClear = null;
+    this.selectedFilters = [];
   }
 }
