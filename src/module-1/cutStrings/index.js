8b72cc6b65ce5069e7cc050a3374b92f4c32721f
ultrayulenka <cherevata.jul@gmail.com>
2021-10-19 13:45:55 +0300

    Fixed module-1 issues

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -2,6 +2,5 @@ export const cutStrings = (arr = []) => {
     if(!arr.length) return [];
     const wordsLength = arr.map(word => word.length);
     const shortestLength = Math.min(...wordsLength);
-    const result = arr.map(word => word.slice(0, shortestLength));
-    return result;
+    return arr.map(word => word.slice(0, shortestLength));
 };
--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,6 +1,9 @@
 export const weirdString = (str = "") => {
     if(!str.length) return "";
     const wordsArr = str.split(" ");
-    const weirdWordsArr = wordsArr.map(word => word.slice(0, word.length-1).toUpperCase() + word[word.length-1]);
+    const weirdWordsArr = wordsArr.map(word => {
+        const lastLetter = word[word.length-1].toLowerCase();
+        return word.slice(0, word.length-1).toUpperCase() + lastLetter;
+    });
     return weirdWordsArr.join(" ");
 };
